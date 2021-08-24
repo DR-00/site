005af19fb08036c4ee3eb277c398a8f90e22e67d
ironc0in <16161930+ironc0in@users.noreply.github.com>
2021-08-24 09:08:06 -0500

    init

---
+++ assets/js/form.js
@@ -0,0 +1,19 @@
+        
+$('.ajax-form').validate({
+    rules: {
+      name: {
+        required: true,
+        minlength: 2
+      },
+      email: {
+        required: true,
+        email: true
+      },
+      message: {
+        required: true,
+        minlength: 5
+    }
+  },
+
+
+  });
